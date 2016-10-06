4dac59b3e6b0c38f5ad11764eff983070ed25541
KOSEKI Kengo <koseki@gmail.com>
2016-10-06 09:52:25 +0900

    Generate circles HTML

---
+++ _src/generate.rb
@@ -0,0 +1,32 @@
+#! /usr/bin/env ruby
+
+require 'yaml'
+require 'erb'
+
+class Generator
+  PROJECT_ROOT = File.expand_path('../..', __FILE__)
+  CIRCLES_DIR = 'data/circles'
+  HTML_DIR = 'circles'
+
+  def execute
+    Dir.chdir(PROJECT_ROOT)
+
+    src_files = []
+
+    erb_src = File.read(File.join('_src/templates/circle.html.erb'))
+    erb = ERB.new(erb_src, nil, '-')
+
+    idx = 0
+    Dir.glob(File.join(CIRCLES_DIR, '*.yml')) do |file|
+      idx += 1
+      words = YAML.load(File.read(file))
+      pattern = File.basename(file, '.yml')
+      html_file = File.join(HTML_DIR, "#{pattern}.html")
+      File.open(html_file, 'w') do |out|
+        out << erb.result(binding)
+      end
+    end
+  end
+end
+
+Generator.new.execute
