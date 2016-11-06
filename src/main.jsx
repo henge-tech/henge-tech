075d650e0c59eef607796b9b91692e7c9927b72e
KOSEKI Kengo <koseki@gmail.com>
2016-11-06 22:36:52 +0900

    Setup webpack and react

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import Hello from './hello.jsx';
+
+render(
+    <div>
+      <Hello />
+    </div>,
+    document.getElementById('app')
+);
