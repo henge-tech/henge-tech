9e2e2ab47fba26e93e062c815d82157ee5f6fe1b
KOSEKI Kengo <koseki@gmail.com>
2018-07-14 18:46:07 +0900

    Switch buttons for "Twitter (multiple)" service

--- src/components/SpeakButtons.jsx
+++ src/components/SpeakButtons.jsx
@@ -7,63 +7,34 @@ export default class SpeakButtons extends React.Component {
     const buttonSize = 30;
     const d = buttonSize / 2.0;
 
-    let styles = {
-      speakButton: {
+    const x = this.props.center.x - d;
+    const y = this.props.center.y - d;
+    const diff = [
+      [-1, -1], [0,  -1], [1,  -1],
+      [-1,  0], [0,   0], [1,   0],
+      [-1,  1], [0,   1], [1,   1],
+    ];
+    let positions = [];
+    for (let i = 0; i < 9; i++) {
+      positions[i] = {
         position: 'absolute',
-        left: this.props.center.x - d + 'px',
-        top: this.props.center.y -d + 'px',
-      },
-      imageTextSwitchButton: {
-        position: 'absolute',
-        left: this.props.center.x - d + 'px',
-        top: this.props.center.y - d - centerButtonMargin + 'px',
-      },
-      changeResolutionButton: {
-        position: 'absolute',
-        left: this.props.center.x - d - centerButtonMargin + 'px',
-        top: this.props.center.y - d - centerButtonMargin + 'px',
-      },
-      toggleResolutionButton: {
-        position: 'absolute',
-        left: this.props.center.x - d + centerButtonMargin + 'px',
-        top: this.props.center.y - d - centerButtonMargin + 'px',
-      },
-      navArrowButton: [
-        {
-          position: 'absolute',
-          left: this.props.center.x - d - centerButtonMargin + 'px',
-          top: this.props.center.y - d + 'px',
-        },
-        {
-          position: 'absolute',
-          left: this.props.center.x - d + centerButtonMargin + 'px',
-          top: this.props.center.y - d + 'px',
-        },
-        {
-          position: 'absolute',
-          left: this.props.center.x - d + 'px',
-          top: this.props.center.y - d + centerButtonMargin + 'px',
-        },
-      ],
+        left: x + (centerButtonMargin * diff[i][0]) + 'px',
+        top:  y + (centerButtonMargin * diff[i][1]) + 'px',
+      };
+    }
 
-      partialSpeakButtons: []
-    };
-
-    const diff = [[1, -1], [1, 1], [-1, 1], [-1, -1]];
-
+    const diff2 = [[1, -1], [1, 1], [-1, 1], [-1, -1]];
+    let aroundPos = [];
     for (let i = 0; i < 4; i++) {
-      const left = this.props.center.x + this.props.r * diff[i][0] - d;
-      const top  = this.props.center.y + this.props.r * diff[i][1] - d;
-      styles.partialSpeakButtons[i] = {
+      const left = this.props.center.x + this.props.r * diff2[i][0] - d;
+      const top  = this.props.center.y + this.props.r * diff2[i][1] - d;
+      aroundPos[i] = {
         position: 'absolute',
         left: left + 'px',
         top:  top + 'px',
-      }
+      };
     }
 
-    let buttonDown;
-    let buttonLeft;
-    let buttonRight;
     const roomType = this.props.floorStatus.roomType();
     const goNextRoom = (direction) => {
       this.props.goNextRoom(this.props.floorStatus, direction, false);
@@ -73,101 +44,49 @@ export default class SpeakButtons extends React.Component {
       window.location.href = '/floors/';
     }
 
-    if (roomType == 'circle') {
-      buttonLeft = (
-        <Button
-        onClick={() => goNextRoom('left')}
-            style={styles.navArrowButton[0]}
-            className="btn-circle"
-          ><Glyphicon glyph="triangle-left" /></Button>
-      );
-      buttonRight = (
-        <Button
-            onClick={() => goNextRoom('right')}
-            style={styles.navArrowButton[1]}
-            className="btn-circle"
-          ><Glyphicon glyph="triangle-right" /></Button>
-      );
-      buttonDown = (
-        <Button
-          onClick={() => goNextRoom('back')}
-            style={styles.navArrowButton[2]}
-            className="btn-circle"
-          ><Glyphicon glyph="triangle-bottom" /></Button>
-      );
+    let buttons = [];
+    if (roomType == 'circle' &&
+        this.props.floorStatus.behaviorName == 'services' &&
+        this.props.floorStatus.behaviorServiceName == 'Twitter (multiple)') {
+      buttons.push(this.circleButton(positions[0], 'check',     () => this.props.onClickImageButton()));
+      buttons.push(this.circleButton(positions[1], 'share-alt', () => this.props.onClickToggleResolutionButton()));
     } else {
-      buttonLeft = (
-        <Button
-            onClick={() => goNextRoom('up')}
-            style={styles.navArrowButton[0]}
-            className="btn-circle"
-          ><Glyphicon glyph="triangle-top" /></Button>
-      );
-      buttonRight = (
-        <Button
-            onClick={() => goNextRoom('down')}
-            style={styles.navArrowButton[1]}
-            className="btn-circle"
-          ><Glyphicon glyph="triangle-bottom" /></Button>
-      );
-      buttonDown = (
-        <Button
-            onClick={() => goFloorIndex()}
-            style={styles.navArrowButton[2]}
-            className="btn-circle"
-          ><Glyphicon glyph="triangle-bottom" /></Button>
-      );
+      buttons.push(this.circleButton(positions[0], 'tint',    () => this.props.onClickChangeResolutionButton()));
+      buttons.push(this.circleButton(positions[1], 'picture', () => this.props.onClickToggleResolutionButton()));
+    }
+    buttons.push(this.circleButton(positions[2], 'font', () => this.props.onClickImageButton()));
+
+    if (roomType == 'circle') {
+      buttons.push(this.circleButton(positions[3], 'triangle-left',   () => goNextRoom('left')));
+      buttons.push(this.circleButton(positions[5], 'triangle-right',  () => goNextRoom('right')));
+      buttons.push(this.circleButton(positions[7], 'triangle-bottom', () => goNextRoom('back')));
+    } else {
+      buttons.push(this.circleButton(positions[3], 'triangle-top',    () => goNextRoom('up')));
+      buttons.push(this.circleButton(positions[5], 'triangle-bottom', () => goNextRoom('down')));
+      buttons.push(this.circleButton(positions[7], 'triangle-bottom', () => goFloorIndex()));
+    }
+
+    buttons.push(this.circleButton(positions[4], 'volume-up', () => this.props.onClickSpeakButton(this.props.floorStatus, -1)));
+    for (let i = 0; i < 4; i++) {
+      const fn = () => this.props.onClickSpeakButton(this.props.floorStatus, i);
+      buttons.push(this.circleButton(aroundPos[i], 'volume-up', fn));
     }
 
     return (
       <div className="speak-buttons">
-        <Button
-            onClick={() => this.props.onClickSpeakButton(this.props.floorStatus, -1)}
-            style={styles.speakButton}
-            className="btn-circle"
-          ><Glyphicon glyph="volume-up" /></Button>
-        <Button
-            onClick={() => this.props.onClickImageButton()}
-            style={styles.toggleResolutionButton}
-            className="btn-circle"
-          ><Glyphicon glyph="font" /></Button>
-        <Button
-            onClick={() => this.props.onClickToggleResolutionButton()}
-            style={styles.imageTextSwitchButton}
-            className="btn-circle"
-          ><Glyphicon glyph="picture" /></Button>
-        <Button
-            onClick={() => this.props.onClickChangeResolutionButton()}
-            style={styles.changeResolutionButton}
-            className="btn-circle"
-          ><Glyphicon glyph="tint" /></Button>
-
-        {buttonLeft}
-        {buttonRight}
-        {buttonDown}
-
-        <Button
-            onClick={() => this.props.onClickSpeakButton(this.props.floorStatus, 0)}
-            style={styles.partialSpeakButtons[0]}
-            className="btn-circle"
-          ><Glyphicon glyph="volume-up" /></Button>
-        <Button
-            onClick={() => this.props.onClickSpeakButton(this.props.floorStatus, 1)}
-            style={styles.partialSpeakButtons[1]}
-            className="btn-circle"
-          ><Glyphicon glyph="volume-up" /></Button>
-        <Button
-            onClick={() => this.props.onClickSpeakButton(this.props.floorStatus, 2)}
-            style={styles.partialSpeakButtons[2]}
-            className="btn-circle"
-          ><Glyphicon glyph="volume-up" /></Button>
-        <Button
-            onClick={() => this.props.onClickSpeakButton(this.props.floorStatus, 3)}
-            style={styles.partialSpeakButtons[3]}
-            className="btn-circle"
-        ><Glyphicon glyph="volume-up" /></Button>
+        {buttons}
       </div>
     );
     // <div style={{background:'red', width:'1px', height:'1px',position:'absolute', top:this.props.center.y, left:this.props.center.x}}><img src="" width="1" height="1"/></div>
+  }
+
+  circleButton(style, glyph, onClick) {
+    return (
+        <Button
+            onClick={onClick}
+            style={style}
+            className="btn-circle"
+          ><Glyphicon glyph={glyph} /></Button>
+    );
   }
 }
